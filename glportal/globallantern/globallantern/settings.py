193421a630c8cf0231ea5b8485cea34fb5cb1319
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-04-25 12:06:26 +0530

    Commit after Initial Deployment

--- glportal/globallantern/globallantern/settings.py
+++ glportal/globallantern/globallantern/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
@@ -34,7 +34,7 @@ SECRET_KEY = ')#(r19v5!@zgqgg2sbzd7=wiho(%f0s*_se)9*tsdne8d@q4$@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gltestingapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
 SITE_ID=1
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -187,4 +187,4 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
