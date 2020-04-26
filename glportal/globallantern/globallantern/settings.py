dad5b883a62f7261ae00b65e904d01a5100bec55
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-04-27 00:36:19 +0530

    NVM: Basic Strut

--- glportal/globallantern/globallantern/settings.py
+++ glportal/globallantern/globallantern/settings.py
@@ -16,6 +16,7 @@ import os
 # DATABASES['default'].update(prod_db)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 
 STATICFILES_DIRS = (
--- glportal/globallantern/globallantern/urls.py
+++ glportal/globallantern/globallantern/urls.py
@@ -42,6 +42,6 @@ urlpatterns = [
     path('addpost_delete/<int:id>',addpost_delete,name='addpostdelete'),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
         
